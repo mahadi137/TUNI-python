6f5dfa9656dbedf3d10a4e99495d3945d4fdbe59
mahadi.eete@gmail.com <E0759578@eaton.ad.etn.com>
2024-03-06 08:58:10 +0200

    bored game first version

---
+++ bored_first_version.py
@@ -0,0 +1,23 @@
+"""
+COMP.CS.100 The Python program.
+Creator: hassan2
+Student id number: tuni.fi:H281750
+"""
+
+
+
+def main ():
+    
+    flag = True
+
+    while flag:
+        answer = input ("Bored? (y/n) ").lower ()
+        if answer == 'y':
+            print ("Well, let's stop this, then.")
+            break
+
+
+
+if __name__ == '__main__':
+    main ()
+

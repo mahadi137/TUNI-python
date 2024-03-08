53debc792a90d19637c79069e09f7ee696ec5d3c
mahadi.eete@gmail.com <E0759578@eaton.ad.etn.com>
2024-03-08 18:45:35 +0200

    precision file

--- precision.py
+++ precision.py
@@ -1,6 +1,13 @@
+"""
+COMP.CS.100 The Python program.
+Creator: hassan2
+Student id number: tuni.fi:H281750
+"""
+
+
 def main():
     PI = 3.14159265358979323846
-    print("The approximate value of pi is", PI, "or, if we want to get specific,", PI)
+    print(f"The approximate value of pi is {PI:0.0f} or, if we want to get specific, {PI:1.4f}")
 
 if __name__ == "__main__":
     main()

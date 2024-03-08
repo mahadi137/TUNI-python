be1e8145f5bb19241dd84c02283b89935690f707
mahadi.eete@gmail.com <E0759578@eaton.ad.etn.com>
2024-03-08 18:46:47 +0200

    fixing space file

---
+++ fixing_space.py
@@ -0,0 +1,16 @@
+"""
+COMP.CS.100 The Python program.
+Creator: hassan2
+Student id number: tuni.fi:H281750
+"""
+
+
+def main():
+    ask_name = input ("Tell us your name: ")
+    print (f"Hey {ask_name}", "the printout formatting is going well!", sep = ", ")
+
+if __name__ == "__main__":
+    main()
+
+
+

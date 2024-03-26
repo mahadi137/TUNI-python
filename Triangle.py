f53ec0ffaea55f4836f2ece0e7f7578ee5f59cb1
mahadi.eete@gmail.com <E0759578@eaton.ad.etn.com>
2024-03-26 12:34:23 +0200

    triangle

--- Triangle.py
+++ Triangle.py
@@ -1,12 +1,26 @@
+"""
+COMP.CS.100 The Python program.
+Creator: hassan2
+Student id number: tuni.fi:H281750
+"""
+
 from math import sqrt
+
+def area(a,b,c):
+    """calculate triangle area"""
+    s = (a+b+c)/2
+    A = sqrt(s*(s-a)*(s-b)*(s-c))
+    return A
 
 
 def main():
-    line = input("Enter the length of the first side: ")
-    line = input("Enter the length of the second side: ")
-    line = input("Enter the length of the third side: ")
+    a = float(input("Enter the length of the first side: "))
+    c = float(input("Enter the length of the second side: "))
+    b = float(input("Enter the length of the third side: "))
 
-    print("The triangle's area is ")
+    area_result = area(a,b,c)
+
+    print(f"The triangle's area is {area_result:1.1f}")
 
 
 if __name__ == "__main__":

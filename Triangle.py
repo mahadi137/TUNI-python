0436bc923bb6bcae2d43a20c00d977db54783226
mahadi.eete@gmail.com <E0759578@eaton.ad.etn.com>
2024-04-13 21:55:33 +0300

    triangle

--- Triangle.py
+++ Triangle.py
@@ -4,23 +4,24 @@ Creator: hassan2
 Student id number: tuni.fi:H281750
 """
 
-from math import sqrt
-
-def area(a,b,c):
-    """calculate triangle area"""
-    s = (a+b+c)/2
-    A = sqrt(s*(s-a)*(s-b)*(s-c))
-    return A
+def calculate_angle(a = 0, b = 90):
+    """
+    Print top angle of a triangle.
+    :param a, b: b default value 90
+    :return: nothing
+    """
+    c = 180 - (a + b)
+    
+    return c
+    
 
 
 def main():
-    a = float(input("Enter the length of the first side: "))
-    c = float(input("Enter the length of the second side: "))
-    b = float(input("Enter the length of the third side: "))
-
-    area_result = area(a,b,c)
-
-    print(f"The triangle's area is {area_result:1.1f}")
+    left = int(input("Enter the left angle: "))
+    right = int(input("Enter the right angle: "))
+    
+    result = calculate_angle(left, right)
+    print(result)    
 
 
 if __name__ == "__main__":

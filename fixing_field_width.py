4566eb8c29b2cb1d90b304037fc7f116b8f7358b
Sayad Mohammad Mahadi Hassan <71854514+mahadi137@users.noreply.github.com>
2025-08-31 08:53:01 +0300

    Update fixing_field_width.py

--- fixing_field_width.py
+++ fixing_field_width.py
@@ -4,11 +4,14 @@ Creator: hassan2
 Student id number: tuni.fi:H281750
 """
 
-def main():
-    ask_number = 100
-    for sequence in range (1, ask_number + 1):
-        print (sequence)
-
-
-if __name__ == "__main__":
-    main()
+def main():                                   
+    for i in range(1, 11):                    
+        for j in range(1, 11):                
+            if j < 11:                        
+                print(f"{i*j:4.0f}", end="")  
+            else:                             
+                print(f"{i*j:2.0f}", end="")  
+        print()                               
+                                              
+if __name__ == "__main__":                    
+    main()                                    
